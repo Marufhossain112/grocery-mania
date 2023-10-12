9ca63b44d66c5c8d9bac741a359f980de96c076c
maruf <dev.maruf112@gmail.com>
2023-10-12 18:12:22 +0600

    dummy featured products done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import Featured from '@/ui/Featured';
+import MyFooter from '@/ui/components/Footer';
+import GMFooter from '@/ui/components/Footer';
 import GMNavbar from '@/ui/components/Navbar';
 import React from 'react';
-
 const HomePage = () => {
   return (
     <div>
       <GMNavbar />
-      
+      {/* <MyFooter /> */}
+      <Featured />
     </div>
   );
 };
-
 export default HomePage;
