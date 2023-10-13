af7a11341195c69a91ee4ec36ae74306ec8b1826
maruf <dev.maruf112@gmail.com>
2023-10-13 09:17:34 +0600

    loading spinner added on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import AboutUs from '@/ui/AboutUs';
 import FAQCard from '@/ui/FAQCard';
 import Featured from '@/ui/Featured';
@@ -7,19 +8,31 @@ import Testimonial from '@/ui/Testimonials';
 import GMFooter from '@/ui/components/Footer';
 import GMNavbar from '@/ui/components/Navbar';
 import ScrollToTopButton from '@/ui/components/ScrollToTopButton';
-import React from 'react';
+import { Spinner } from 'flowbite-react';
+import React, { useEffect, useState } from 'react';
 const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <div>
-      <GMNavbar />
-      <Featured />
-      <Testimonial />
-      <Gallery />
-      <AboutUs />
-      <FAQCard />
-      <Newsletter />
-      <ScrollToTopButton />
-      <GMFooter />
+      {
+        isLoading ? <div style={{height:"100vh"}} className="flex justify-center items-center">
+          <Spinner size="lg" aria-label="Center-aligned spinner example" />
+        </div> : <div><GMNavbar />
+          <Featured />
+          <Testimonial />
+          <Gallery />
+          <AboutUs />
+          <FAQCard />
+          <Newsletter />
+          <ScrollToTopButton />
+          <GMFooter /></div>
+      }
     </div>
   );
 };
